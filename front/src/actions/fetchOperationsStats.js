01cbcc265260dc12942242b7c6fb8ad2064a6664
Sergey Kudasov <lulunevermind@gmail.com>
2018-01-15 12:16:03 +0300

    упрощение ошибок

--- front/src/actions/fetchOperationsStats.js
+++ front/src/actions/fetchOperationsStats.js
@@ -1,6 +1,15 @@
 import {RECEIVE_OPERATIONS_USER, RECEIVE_OPERATIONS_USER_ERROR} from "../constants/ActionTypes";
 import {appLoading} from "./index";
 
+const checkIdIdpPair = (userid, useridp, dispatch) => {
+    if (userid === "" ^ useridp === "") {
+        let obj = {};
+        obj.error = "Введите IDP ID и IDP Мнемонику, либо оставьте оба поля пустыми";
+        dispatch(receiveOperationsStatsSearchError(obj));
+        return true
+    }
+};
+
 export const receiveOperationsStats = (operations) => ({
     type: RECEIVE_OPERATIONS_USER,
     operations: operations
@@ -10,15 +19,6 @@ export const receiveOperationsStatsSearchError = (operations) => ({
     type: RECEIVE_OPERATIONS_USER_ERROR,
     operations: operations
 });
-
-const checkIdIdpPair = (userid, useridp, dispatch) => {
-    if (userid === "" ^ useridp === "") {
-        let obj = {};
-        obj.error = "Введите IDP ID и IDP Мнемонику, либо оставьте оба поля пустыми";
-        dispatch(receiveOperationsStatsSearchError(obj));
-        return true
-    }
-};
 
 export const fetchOperationsStats = (userid, useridp, pageSize) => async (dispatch, getState) => {
     if (checkIdIdpPair(userid, useridp, dispatch)) {
